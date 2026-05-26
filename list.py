c662a4198660926440bf68b9aa77280be6a8d01a
0611malhar <malhar0611@gmail.com>
2026-05-26 14:09:49 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,29 @@
+# wap to store 7 elements entered by user
+
+# a = []
+# for i in range (7):
+#     x = input()
+#     a.append(x)
+
+# input and view six entries
+# a = []
+# for i in range(6):
+#     x = int(input())
+#     a.append(x)
+# for i in range(6):
+#     print(a[i])
+
+# sum of all items of list
+# a = [12,31,85,98]
+# s = 0
+# for i in range(len(a)):
+#     s+=a[i]
+# print(s)
+
+# count no. of 0s
+# a = [7,0,8,0,0,9]
+# s = 0
+# for i in range(len(a)):
+#     if(a[i]==0):
+#         s+=1
+# print(s)
---
+++ set_dictionary.py
@@ -0,0 +1,18 @@
+#Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+# a = {1:"a",2:"b",3:"c",4:"d"}
+# print(a[1])
+
+# 2. Write a program to input eight numbers from the user and display all the unique numbers (once).
+# a = set()
+# for i in range (8):
+#     x = int(input())
+#     a.add(x)
+# print(a)
+
+# 6. Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
+# names = {}
+# for i in range(4):
+#     n = input("Enter name:")
+#     l = input("Enter lang:")
+#     names[n] = l
+# print(names)
